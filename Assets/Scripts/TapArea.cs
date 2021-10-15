cc713c491973dd2728307cdd3adc30c7e565b86e
AvinChailiSalim <avinchailisalim@gmail.com>
2021-10-15 16:45:59 +0700

    First commit

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
